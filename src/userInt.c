1d4ccf67b28601cc3245afbec70afd894aac84eb
student <sghernandez5@miners.utep.edu>
2020-09-21 02:11:02 -0600

    c hates me i hate c

--- src/userInt.c
+++ src/userInt.c
@@ -5,23 +5,45 @@
 
 int main()
 {
- printf("Hello please enter your sentence: ");
-  char str1[50];
-  scanf("%[^\n]", str1,50); 
-  printf("%s\n", str1);
+  List *history = init_history();
+  int choice;
+  printf("Press 0 to view history\n");
+  printf("Press 1 to recall at certain history.\n");
+  printf("Press 2 to enter new sentence\n");
+  printf("Press any key to exit");
 
-  char *pstr1 = str1;
 
-  printf("%d\n", space_char(str1[0]));
-  printf("%d\n",non_space_char(str1[0])); 
-  printf("%d\n", count_words(pstr1));
 
-  char **Tokensr = tokenize(pstr1);
+  do {
+  scanf("%d", &choice);
+  if (choice==0){
+  print_history(history);
+  continue;
+  }
+  else if (choice==1){
+    int r;
+    printf("Enter num"); 
+    int iD = scanf("%d",&r);
+    get_history(history,r);
+    continue; 
+  }
+  else if(choice ==2){
+    printf("Enter string"); 
+    char newString[80];
+    scanf("%[^\n]", newString,80);
+    char *pString = newString; 
+    char **Tokensr = tokenize(pString);
+    int i =0;
+    while(Tokensr[i]){
+      add_history(history, Tokensr[i]);
+      i++;
+      }
+    continue; 
+  }
+  else{
+    break; 
+  }
+  }while(choice != 3);
   
-  print_tokens(Tokensr);
-
-  free_tokens(Tokensr);
-  
-  print_tokens(Tokensr); 
   return 0;
 }
